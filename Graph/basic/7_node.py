16ad984c862bdd78ad1e4624db4a3d6473e5f96a
enamulbokshi <enamulbokshi@gmail.com>
2025-04-02 15:27:55 +0600

    Implement DFS and BFS algorithms for graph traversal

--- Graph/basic/7_node.py
+++ Graph/basic/7_node.py
@@ -24,7 +24,48 @@ adj_list = defaultdict(list)
 for u,v in edge_list:
     adj_list[u].append(v)
 
-print(adj_list)
+print(adj_list) #{0: [1, 2], 1: [3], 3: [4], 4: [7], 7: [2], 2: [5, 6]}
+
 
 #dfs using recursion
+seen = set()
+start = 0
+seen.add(start)
+
+def dfs(node):
+    print(node)
+    for ng in adj_list[node]:
+        if ng not in seen:
+            seen.add(ng)
+            dfs(ng)
+
+# dfs(start)
+
+#dfs using stack
+seen = set()
+start = 0
+stack = [start]
+seen.add(start)
+while stack:
+    node = stack.pop()
+    print(node)
+    for ng in adj_list[node]:
+        if ng not in seen:
+            seen.add(ng)
+            stack.append(ng)
+
+# bfs using queue
+from collections import deque
+visited = set()
+start = 0
+queue = deque([start])
+while queue:
+    node = queue.popleft()
+    print(node)
+    for ng in adj_list[node]:
+        if ng not in visited:
+            visited.add(ng)
+            queue.append(ng)
+
+
 
